58a77c4b71638573fe756b44728d9598b7c6eedc
Muhammad Ahmad Sohail <ahmaddd.sohail759@gmail.com>
2025-03-24 11:07:49 +0500

    GetInTouch change

--- src/app/components/GetInTouch/GetInTouch.jsx
+++ src/app/components/GetInTouch/GetInTouch.jsx
@@ -6,8 +6,8 @@ const GetInTouch = () => {
     <section className='flex flex-col w-full p-10 pt-9 justify-center items-center bg-[#F2F2F2]'>
     <div className='flex w-full m-10 justify-center items-center  rounded-2xl min-h-80 bg-[#002049]'>
     <div className='flex flex-col font-poppins justify-center items-center space-y-4 text-white'>
-        <p className='text-4xl font-semibold'>We'd love to hear from you!</p>
-        <p className='text-lg font-medium'>Whether you </p>
+        <p className='text-4xl font-medium'>We'd love to hear from you!</p>
+        <p className='text-base font-light text-center'>Whether you have a question about features, trials, pricing, need a demo, or anything else, our team is <br/> ready to answer all you questions</p>
     </div>
     </div>
         </section>
