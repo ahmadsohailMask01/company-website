43a0c01286419bb1cf67977b5ba5b10b7bd99cb3
Muhammad Ahmad Sohail <ahmaddd.sohail759@gmail.com>
2025-03-28 10:59:59 +0500

    Scroll to top button added

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
-        
+        <ScrollToTopButton/>
       </body>
     </html>
   );
