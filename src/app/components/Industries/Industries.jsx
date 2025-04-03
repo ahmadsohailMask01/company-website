7203b6869df22d10a38879acb483b57252a7a0ce
Muhammad Ahmad Sohail <ahmaddd.sohail759@gmail.com>
2025-04-03 12:46:06 +0500

    New margins added

--- src/app/components/Industries/Industries.jsx
+++ src/app/components/Industries/Industries.jsx
@@ -8,42 +8,40 @@ const Industries = () => {
         setWidth(window.innerWidth);
     }, []);
   return (
-    <section className='flex flex-col justify-center items-center bg-white w-full p-4 py-8 font-poppins space-y-5'>
-        <div className=' text-4xl font-extrabold text-black'>
-            <p>WHO WE <font className='text-[#2F7af7]'>HELP</font></p>
-        </div>
+    <section className='flex flex-col justify-center items-center bg-[#F2F2F2] w-full font-poppins'>
         <div className=' text-black text-center font-medium tracking-wide text-lg lg:w-[40%]'>
-            <p className='text-base font-light'>Delivering tailored digital solutions to drive innovation & efficiency across diverse industries.</p>
+        <p className='leading-6 font-extrabold text-4xl mt-8 mb-4'>WHO WE <span className='text-[#2F7af7]'>HELP</span></p>
+            <p className='text-base font-light leading-4 my-4'>Delivering tailored digital solutions to drive innovation & efficiency across diverse industries.</p>
         </div>
-        <div className={`grid ${width<=430?'grid-cols-1':'grid-cols-2'} md:grid-cols-5 lg:w-[86%] rounded-2xl py-6 shadow-lg bg-[#F2F2F2]`}>
-            <div className='flex flex-col px-12 justify-center items-start space-y-4 border-r-gray-300 border-r'>
-                <img src='/images/Anime-1.gif' className='w-16 rounded-[50px]'/>
-                <p className='font-bold w-36 text-start text-lg'
+        <div className={`grid ${width<=430?'grid-cols-1':'grid-cols-2'} md:grid-cols-5 lg:w-[86%] rounded-2xl mb-8 p-8 bg-white`}>
+            <div className='flex flex-col justify-center items-start border-r-gray-300 border-r'>
+                <img src='/images/Anime-1.gif' className='w-16 rounded-[50px] mb-4'/>
+                <p className='font-bold w-36 text-start text-lg leading-5'
                 >Business Consultancy</p>
             </div>
-            <div className='flex flex-col px-12  justify-center  items-start space-y-4 border-r-gray-300 lg:border-r'>
-            <img src='/images/2.png' className=' w-20 rounded-[20px]'/>
-                <p className='font-bold w-36 text-start text-lg
+            <div className='flex flex-col  justify-center  items-start border-r-gray-300 pl-8 lg:border-r'>
+            <img src='/images/2.png' className=' w-20 rounded-[20px] mb-4'/>
+                <p className='font-bold w-36 text-start text-lg leading-5
                 '>Technology Consultancy</p>
             </div>
-            <div className='flex flex-col px-12  justify-center items-start space-y-4 border-r-gray-300 border-r'>
-            <img src='/images/Anime-3.gif' className=' w-20 rounded-[14px]'/>
-                <p className='font-bold w-36 text-start text-lg
+            <div className='flex flex-col  justify-center items-start border-r-gray-300 pl-8 border-r'>
+            <img src='/images/Anime-3.gif' className=' w-20 rounded-[14px] mb-4'/>
+                <p className='font-bold w-36 text-start text-lg leading-5
                 '>ERP Strategy & Implementation</p>
             </div>
-            <div className='flex flex-col px-12  justify-center  items-start space-y-4 border-r-gray-300 lg:border-r'>
-            <img src='/images/4.png' className=' w-20 rounded-[20px]'/>
-                <p className='font-bold w-36 text-start text-lg
+            <div className='flex flex-col  justify-center  items-start border-r-gray-300 pl-8 lg:border-r'>
+            <img src='/images/4.png' className=' w-20 rounded-[20px] mb-4'/>
+                <p className='font-bold w-36 text-start text-lg leading-5
                 '>Internet of Things (IOT)</p>
             </div>
-            <div className='flex flex-col px-12  justify-center lg:ml-[-10]  items-start space-y-4 '>
-            <img src='/images/Anime-5.gif' className=' w-20 rounded-[20px]'/>
-                <p className='font-bold w-36 text-start text-lg
+            <div className='flex flex-col  justify-center lg:ml-[-10] pl-8 items-start'>
+            <img src='/images/Anime-5.gif' className=' w-20 rounded-[20px] mb-4'/>
+                <p className='font-bold w-36 text-start text-lg leading-5
                 '>Education & Training</p>
             </div>
         </div>
-        <div className='bg-[#2f7af7] px-4 py-2 mb-2 rounded-[100px] hover:bg-[#255fc1] transition-all duration-300'>
-            <Button label='Explore more industries' className='cursor-pointer text-white'/>
+        <div className='bg-[#2f7af7] rounded-[100px] hover:bg-[#255fc1] mb-8 transition-all duration-300'>
+            <Button label='Explore more industries' className='cursor-pointer text-white px-4 py-2'/>
         </div>
         </section>
   )
