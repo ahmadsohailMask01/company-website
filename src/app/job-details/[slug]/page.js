418bf377357384d81d14cfeb71ef9aee7317c742
Muhammad Ahmad Sohail <ahmaddd.sohail759@gmail.com>
2025-04-04 10:30:03 +0500

    minor change

--- src/app/job-details/[slug]/page.js
+++ src/app/job-details/[slug]/page.js
@@ -3,7 +3,7 @@ import CareersDetail from '@/app/components/CareersDetail/CareersDetail';
 import { useParams } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const DetailsPage = () => {
     const params = useParams();
     const slug = params.slug;
   return (
@@ -11,4 +11,4 @@ const page = () => {
   )
 }
 
-export default page
+export default DetailsPage
