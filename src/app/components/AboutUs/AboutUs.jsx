67d60a29c23f6bfd119b2531c5084e35efa23515
Muhammad Ahmad Sohail <ahmaddd.sohail759@gmail.com>
2025-04-10 13:20:27 +0500

    design wave added

--- src/app/components/AboutUs/AboutUs.jsx
+++ src/app/components/AboutUs/AboutUs.jsx
@@ -10,8 +10,8 @@ export default function AboutPage() {
     <div className="bg-[#f0f6ff] text-gray-800 font-poppins">
       {/* Hero Section */}
       <section className="relative bg-[#002049] text-white py-36 px-6 overflow-hidden">
-        <div className="absolute top-0 left-0 w-80 h-80 bg-white opacity-10 rounded-full blur-3xl animate-pulse" />
-        <div className="absolute bottom-0 right-0 w-96 h-96 bg-white opacity-10 rounded-full blur-3xl animate-pulse" />
+        <div className="absolute z-1 top-0 left-0 w-50 h-50 bg-white opacity-10 rounded-full blur-3xl animate-pulse" />
+        <div className="absolute z-1 bottom-0 right-0 w-50 h-50 bg-white opacity-10 rounded-full blur-3xl animate-pulse" />
         <div className="flex flex-col items-start relative max-w-4xl mx-auto text-center">
           <span className="text-4xl inline-block font-medium mb-6  tracking-wide">
             Tech That <span className="text-[#2f7af7]">Transforms</span>.
@@ -21,9 +21,6 @@ export default function AboutPage() {
             Biafotech stands as a beacon of excellence, offering a comprehensive
             suite of services. Our technology-agnostic approach allows us to
             provide cutting-edge solutions tailored to your business needs.
-            Whether it's business consultancy, technology consultancy, ERP
-            strategy and implementation, or specialized education and training,
-            we excel in delivering solutions that drive success.
           </p>
           <Button
             label="Get in Touch"
@@ -33,17 +30,17 @@ export default function AboutPage() {
         </div>
 
         {/* Full-width Wave SVG */}
-        <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none">
+        <div className="absolute bottom-[-60] left-0 bg-white w-full overflow-hidden leading-none">
           <svg
             viewBox="0 0 1440 320"
             preserveAspectRatio="none"
-            className="w-full h-32"
+            className="w-full h-50"
           >
             <path
-              fill="white"
-              fillOpacity="1"
-              d="M0,160L60,165.3C120,171,240,181,360,176C480,171,600,149,720,133.3C840,117,960,107,1080,117.3C1200,128,1320,160,1380,176L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
-            />
+  fill="#002049"
+  fill-opacity="1"
+  d="M0,192L120,165.3C240,139,480,85,720,85.3C960,85,1200,139,1320,165.3L1440,192L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
+/>
           </svg>
         </div>
       </section>
